beeb1e2a9ead1ebc7d046d40777cb5303f84d97e
James Formica <james.formica@gmail.com>
2017-11-02 13:28:40 +1100

    trying out css modules

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import NavigationLink from './NavigationLink';
-import '../css/Navigation.css';
+import navStyles from '../css/Navigation.css';
 
 const Navigation = () => {
     return (
         <nav className="navigation">
-            <span className="navigation-hamburger" onClick={(e) => alert('Who named this a hamburger? Now I\'m hungry :(')}>|||</span>
+            <span className={navStyles.navigationHamburger} onClick={(e) => alert('Who named this a hamburger? Now I\'m hungry :(')}>|||</span>
             <NavigationLink text={"Buy"} alert={"I dont actually work"} extraClass={"navigation-link-stay"} />
             <NavigationLink text={"Rent"} alert={"Neither do I!"} extraClass={"navigation-link-stay"} />
             <NavigationLink text={"Invest"} alert={"I thought you would have gotten the picture by now..."} extraClass={"navigation-link-stay"} />
